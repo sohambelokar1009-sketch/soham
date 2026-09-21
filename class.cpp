2579948cd67db623545ab058b3ff8cd01e88a4c4
sohambelokar1009-sketch <sohambelokar1009@gmail.com>
2026-09-21 11:32:50 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int roll1, roll2, roll3, roll4, roll5;
+
+	cout<<"Enter Roll no of First Student:";
+	cin>>roll1;
+
+	cout<<"Enter Roll no of Second Student:";
+	cin>>roll2;
+
+	cout<<"Enter Roll no of Third Student:";
+	cin>>roll3;
+
+	cout<<"Enter Roll no of Forth Student:";
+	cin>>roll4;
+
+	cout<<"Enter Roll no of Fifth Student:";
+	cin>>roll5;
+
+	//Display
+ 
+	cout<<"\n-----------Roll No-----------\n";
+	cout<<roll1<<endl;
+	cout<<roll2<<endl;
+	cout<<roll3<<endl;
+	cout<<roll4<<endl;
+	cout<<roll5<<endl;
+
+	return 0;
+} 
+
+
